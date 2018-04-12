bec83ee654e7653592697f70411be8f339cd8fc8
WosAleks <wosa2@yahoo.com>
2018-04-12 13:35:28 +0100

    Created entities and relationships

---
+++ src/Entity/User.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="user", orphanRemoval=true)
+     */
+    private $reviews;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\IceCream", mappedBy="user", orphanRemoval=true)
+     */
+    private $icecreams;
+
+    public function __construct()
+    {
+        $this->reviews = new ArrayCollection();
+        $this->icecreams = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function setRoles($roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Review[]
+     */
+    public function getReviews(): Collection
+    {
+        return $this->reviews;
+    }
+
+    public function addReview(Review $review): self
+    {
+        if (!$this->reviews->contains($review)) {
+            $this->reviews[] = $review;
+            $review->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReview(Review $review): self
+    {
+        if ($this->reviews->contains($review)) {
+            $this->reviews->removeElement($review);
+            // set the owning side to null (unless already changed)
+            if ($review->getUser() === $this) {
+                $review->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|IceCream[]
+     */
+    public function getIcecreams(): Collection
+    {
+        return $this->icecreams;
+    }
+
+    public function addIcecream(IceCream $icecream): self
+    {
+        if (!$this->icecreams->contains($icecream)) {
+            $this->icecreams[] = $icecream;
+            $icecream->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIcecream(IceCream $icecream): self
+    {
+        if ($this->icecreams->contains($icecream)) {
+            $this->icecreams->removeElement($icecream);
+            // set the owning side to null (unless already changed)
+            if ($icecream->getUser() === $this) {
+                $icecream->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
